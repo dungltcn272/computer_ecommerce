f9fa9150e134c30a0c8d7e7de5123f89599aa3a2
Dung <dungltcn272@gmail.com>
2025-04-19 22:32:28 +0700

    Cho user sửa thông tin như name, phone của bản thân

--- src/main/kotlin/routes/UserRoutes.kt
+++ src/main/kotlin/routes/UserRoutes.kt
@@ -48,18 +48,27 @@ fun Route.userRoutes(userService: UserService) {
         }
     }
 
+    get("/users/{id}/address") {
+
+        val adminId = UUID.fromString(call.principal<UserIdPrincipal>()!!.name)
+        val userId = UUID.fromString(call.parameters["id"])
+        val address = userService.adminGetUserAddress(userId, adminId)
+        call.respond(HttpStatusCode.OK, address)
+
+    }
+
     route("/user") {
-        get("{id}/address") {
-            val adminId = UUID.fromString(call.principal<UserIdPrincipal>()!!.name)
-            val userId = UUID.fromString(call.parameters["id"])
-            val address = userService.adminGetUserAddress(userId, adminId)
+
+        get("/address") {
+            val userId = UUID.fromString(call.principal<UserIdPrincipal>()!!.name)
+            val address = userService.getAddress(userId)
             call.respond(HttpStatusCode.OK, address)
         }
 
         put {
             val userId = UUID.fromString(call.principal<UserIdPrincipal>()!!.name)
             val userRequest = call.receive<UpdateUserRequest>()
-            val success = userService.updateUser(userId, userRequest.fullName, userRequest.phone )
+            val success = userService.updateUser(userId, userRequest.fullName, userRequest.phone)
             call.respond(HttpStatusCode.OK, mapOf("success" to success))
         }
 
